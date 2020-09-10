3a0162e0a1afc4d7d10c76be16be31032a27b32b
Uni-Stark <kgo0748@gmail.com>
2020-09-10 20:01:45 +0900

    First Commit

---
+++ app/src/main/java/kr/uni/yeoeat/Intro/ActivitySplash.java
@@ -0,0 +1,16 @@
+package kr.uni.yeoeat.Intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.uni.yeoeat.R;
+
+public class ActivitySplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
