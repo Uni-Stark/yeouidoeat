f59de9d4215be177be1443060ff0695f7bb7d3d1
KimGwang-Un <kgo0747@catholic.ac.kr>
2020-09-12 00:43:55 +0900

    Bottom Nav 추가 / Firestore 연동

--- app/src/main/java/kr/uni/yeoeat/ui/MainActivity.java
+++ app/src/main/java/kr/uni/yeoeat/ui/MainActivity.java
@@ -1,16 +1,28 @@
 package kr.uni.yeoeat.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import kr.uni.yeoeat.R;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
